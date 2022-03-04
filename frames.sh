47d3f4103b62ea40de1b9699018e2895358c6d94
Marcos-eca <marcos.eca.2020@unifei.edu.br>
2022-03-03 23:45:35 -0300

    up_model

--- frames.sh
+++
@@ -1,5 +0,0 @@
-source /opt/ros/foxy/setup.bash
-. install/setup.bash
-ros2 run tf2_tools view_frames.py
-ros2 run tf2_tools view_frames
-evince frames.pdf
--- golfinho.sh
+++
@@ -1,7 +0,0 @@
-rm -rf build log install
-source /opt/ros/foxy/setup.bash
-colcon build
-. install/setup.bash
-
-ros2 launch golfinho_robsic golfinho_gazebo_cartograph.launch.py
-
